d075fd6ed7f0fc14a47bd9debf4379c014e93883
kambizzandi <kambizzandi@gmail.com>
2022-04-13 17:44:36 +0430

    intermediate commit...

--- sms/SendSms.php
+++ sms/SendSms.php
@@ -3,15 +3,46 @@
 
 include_once(__DIR__ . "/../AlertManager.php");
 
-$config = require(__DIR__ . "/../config/Alerting.conf.php");
+function SendSms() {
 
-$smsGateway = AlertManager::instantiateClass($config["smsgateway"]);
+    $smsGateway = AlertManager::instantiateClassByConfigName("smsgateway");
 
-$db = AlertManager::instantiateClass($config["db"]);
+    $db = AlertManager::db();
 
-$data = $db->selectAll("SELECT * FROM tblAlerts");
+    $data = $db->selectAll(<<<SQL
+        SELECT *
+          FROM tblAlerts
+         WHERE alr_altCode = 'approveMobile'
+           AND alrStatus = 'N'
+           AND alrLockedAt IS NULL
+      ORDER BY alrCreateDate ASC
+         LIMIT 10
+SQL
+    );
 
-print_r($data);
+    print_r($data);
+
+    if (empty($data))
+        return;
+
+    $ids = array_map(function ($ar) { return $ar["alrID"]; }, $data);
+
+    print_r($ids);
+
+    if (empty($data))
+        throw new \Exception("Error in gathering alerts ids");
+
+    //lock items
+    $rowsCount = $db->execute(strtr(<<<SQL
+        UPDATE tblAlerts
+           SET alrLockedAt = NOW()
+         WHERE alrID IN (:ids)
+SQL
+    , [
+        ':ids' => implode(',', $ids),
+    ]));
+
+    print_r([implode(',', $ids), $rowsCount]);
 
 
 
@@ -23,4 +54,8 @@ print_r($data);
 
 
 
-// $smsGateway->send("a", "b", "c");
+
+    // $smsGateway->send("a", "b", "c");
+}
+
+SendSms();
