a07a9a410ad079ec3565bab61323848af15a9e46
kambizzandi <kambizzandi@gmail.com>
2022-04-13 17:43:42 +0430

    intermediate commit...

--- common/db/Command.php
+++ common/db/Command.php
@@ -6,14 +6,16 @@ namespace Targoman\AlertManager\common\db;
 class Command {
     public $db;
     public $qry;
+    public $params=[];
     public $pdoStatement;
 
-    public function __construct($_db, $_qry) {
+    public function __construct($_db, $_qry, $_params=[]) {
         if (empty($_qry))
             throw new \Exception("Query string is empty");
 
         $this->db = $_db;
         $this->qry = $_qry;
+        $this->params = $_params;
     }
 
     public function prepare() {
@@ -21,6 +23,22 @@ class Command {
             return;
 
         $this->pdoStatement = $this->db->getPDO()->prepare($this->qry);
+
+        if (empty($this->params) == false) {
+            foreach ($this->params as $k => $v) {
+                if (is_array($v))
+                    $this->pdoStatement->bindValue($k, implode(',', $v));
+                else {
+                    $paramType = \PDO::PARAM_STR;
+
+                    if (is_numeric($v))
+                        $paramType = \PDO::PARAM_INT;
+                    // elseif ...
+
+                    $this->pdoStatement->bindValue($k, $v, $paramType);
+                }
+            }
+        }
     }
 
     public function queryAll() {
@@ -39,6 +57,15 @@ class Command {
         $this->pdoStatement->execute();
         $result = $this->pdoStatement->fetch();
         $this->pdoStatement->closeCursor();
+
+        return $result;
+    }
+
+    public function execute() {
+        $this->prepare();
+
+        $this->pdoStatement->execute();
+        $result = $this->pdoStatement->rowCount();
 
         return $result;
     }
