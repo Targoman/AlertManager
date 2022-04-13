935a247f8a5382d90ca7e14e91c90a156312924c
kambizzandi <kambizzandi@gmail.com>
2022-04-13 17:43:25 +0430

    intermediate commit...

--- AlertManager.php
+++ AlertManager.php
@@ -4,6 +4,15 @@
 require __DIR__ . "/Autoloader.php";
 
 class AlertManager extends Autoloader {
+
+    private static $config = null;
+    public static function config() {
+        if (self::$config == null)
+            self::$config = require(__DIR__ . "/config/Alerting.conf.php");
+
+        return self::$config;
+    }
+
     public static function instantiateClass($_config) {
         $className = array_shift($_config);
 
@@ -20,6 +29,33 @@ class AlertManager extends Autoloader {
 
         return $class;
     }
+
+    public static function instantiateClassByConfigName($_configName) {
+        $config = self::config();
+
+        if (empty($config[$_configName])) {
+            throw new \Exception("$_configName not configured");
+            return;
+        }
+
+        $object = AlertManager::instantiateClass($config[$_configName]);
+
+        if (is_null($object)) {
+            throw new \Exception("Could not create $_configName");
+            return;
+        }
+
+        return $object;
+    }
+
+    private static $db = null;
+    public static function db() {
+        if (self::$db == null)
+            self::$db = self::instantiateClassByConfigName("db");
+
+        return self::$db;
+    }
+
 }
 
 AlertManager::$baseNamespace = "Targoman";
